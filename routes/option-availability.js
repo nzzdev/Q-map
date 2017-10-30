6868886ba43f55ecb20d674b7b87535a41951927
Beni <beni.buess@nzz.ch>
2017-10-30 17:57:51 +0100

    feat(options): add option labelsBelowMapOneRow if labelsBelowMap is true

---
+++ routes/option-availability.js
@@ -0,0 +1,26 @@
+const Boom = require('boom');
+const Joi = require('joi');
+
+function hasLabelsBelowMap(item) {
+  return (item.options.labelsBelowMap === true);
+}
+
+module.exports = {
+  method: 'POST',
+  path: '/option-availability/{optionName}',
+  config: {
+    validate: {
+      payload: Joi.object()
+    },
+    cors: true
+  },
+  handler: function(request, reply) {
+    if (request.params.optionName === 'labelsBelowMapOneRow') {
+      return reply({
+        available: hasLabelsBelowMap(request.payload)
+      }).type('application/json');
+    }
+
+    return reply(Boom.badRequest());
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 module.exports = [
   require('./rendering-info/html-js.js'),
+  require('./option-availability.js'),
   require('./stylesheet.js'),
   require('./locales.js')
 ].concat(require('./scripts.js'), require('./schema.js'));
