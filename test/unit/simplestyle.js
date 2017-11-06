48b5bc7a5ee78c1e84ca2eb6f95da546998709c1
Sharon Funke <sharon.funke@nzz.ch>
2017-11-06 10:53:29 +0100

    Fix unit test

--- test/unit/simplestyle.js
+++ test/unit/simplestyle.js
@@ -7,19 +7,18 @@ const properties = {
 
 describe('simplestyle to leaflet path style transformation', function() {
   it('should translate all properties correctly', function() {
-    let style = simplestyleToLeafletStyle(properties);
+    let style = simplestyleToLeafletStyle(properties, 'Polygon');
     expect(style.fillOpacity).to.be.equal(0.5);
   });
 
   it('should remove simplestyle properties after mapping', function() {
-    let style = simplestyleToLeafletStyle(properties);
+    let style = simplestyleToLeafletStyle(properties, 'Polygon');
     expect(style.hasOwnProperty('fill-opacity')).to.be.equal(false);
   });
 
   it('should apply defaults for undefined simplestyle properties', function() {
-    let style = simplestyleToLeafletStyle(properties);
-    expect(style.color).to.be.equal('#c31906');
+    let style = simplestyleToLeafletStyle(properties, 'Polygon');
+    expect(style.weight).to.be.equal(0);
     expect(style.fillColor).to.be.equal('#c31906');
-    expect(style.opacity).to.be.equal(1);
   });
 });
