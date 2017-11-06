034dfce5a29821cf215073e2f995837a64250b30
Sharon Funke <sharon.funke@nzz.ch>
2017-11-06 11:00:20 +0100

    Make mapping function call global in simplestyle unit test

--- test/unit/simplestyle.js
+++ test/unit/simplestyle.js
@@ -5,19 +5,19 @@ const properties = {
   'fill-opacity': 0.5
 };
 
+let style = simplestyleToLeafletStyle(properties, 'Polygon');
+console.log(style);
+
 describe('simplestyle to leaflet path style transformation', function() {
   it('should translate all properties correctly', function() {
-    let style = simplestyleToLeafletStyle(properties, 'Polygon');
     expect(style.fillOpacity).to.be.equal(0.5);
   });
 
   it('should remove simplestyle properties after mapping', function() {
-    let style = simplestyleToLeafletStyle(properties, 'Polygon');
     expect(style.hasOwnProperty('fill-opacity')).to.be.equal(false);
   });
 
   it('should apply defaults for undefined simplestyle properties', function() {
-    let style = simplestyleToLeafletStyle(properties, 'Polygon');
     expect(style.weight).to.be.equal(0);
     expect(style.fillColor).to.be.equal('#c31906');
   });
