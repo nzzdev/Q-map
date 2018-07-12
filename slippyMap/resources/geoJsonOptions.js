d96930fd1884a477d5ae3d3acec2dc45e260e42a
Manuel Roth <manuel.roth@nzz.ch>
2018-07-12 11:44:22 +0200

    Add comment explaining the antimeridian workaround

--- slippyMap/resources/geoJsonOptions.js
+++ slippyMap/resources/geoJsonOptions.js
@@ -24,6 +24,10 @@ export default {
     let lng = coords[0];
     const pacificBounds = Leaflet.latLngBounds([[-180, -90], [-125, 90]]);
 
+    // If a point has a negative longitude value and is within the pacific area (pacificBounds) it is projected to a positive value.
+    // This allows to show features left and right of the antimeridian line to be shown next to each other.
+    // See the animeridian fixture data for an example and this awesome blog post explaining the problem in depth:
+    // https://macwright.org/2016/09/26/the-180th-meridian.html
     if (lng < 0 && pacificBounds.contains(coords)) {
       lng = Leaflet.Util.wrapNum(lng, [0, 360], true);
     }
